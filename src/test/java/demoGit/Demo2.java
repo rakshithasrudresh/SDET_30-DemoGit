5c8da242845e498d58171b5cf425993c4c4fab68
Rakshitha <Rakshitha@DESKTOP-QNLDC4R>
2022-03-13 12:50:43 +0530

    Commit Practice

---
+++ src/test/java/demoGit/Demo2.java
@@ -0,0 +1,27 @@
+package demoGit;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("User 1-First Commit");
+		System.out.println("User 2-First commit");
+		System.out.println("this will arise conflict");
+		
+		System.out.println("pushing	 codee");
+		System.out.println("user-1 -first ");
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("push deploy code");
+	}	
+
+}
